30d0b3e1b9fa72d1655e2aedd4e27965d1a5c682
capelaum <thecapellett@gmail.com>
2023-03-02 15:16:40 -0300

    ✨ component: Box

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,16 @@
+import { Box, BoxProps } from '@capelaum/ignite-react-05-design-system-react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: (
+      <>
+        <span>Testando o elemento Box</span>
+      </>
+    ),
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
