a43493322c1b8919de59d49f3389391a2dc2ce68
capelaum <thecapellett@gmail.com>
2023-03-02 17:09:14 -0300

    ✨ component: Checkbox

---
+++ packages/react/src/components/Checkbox/styles.ts
@@ -0,0 +1,64 @@
+import * as Checkbox from '@radix-ui/react-checkbox'
+import { keyframes, styled } from '~/styles/stitches.config'
+
+export const CheckboxContainer = styled(Checkbox.Root, {
+  all: 'unset',
+  width: '$6',
+  height: '$6',
+  backgroundColor: '$gray900',
+  borderRadius: '$xs',
+  lineHeight: 0,
+  cursor: 'pointer',
+  overflow: 'hidden',
+  boxSizing: 'border-box',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  border: '2px solid $gray900',
+
+  '&:focus': {
+    border: '2px solid $ignite300',
+  },
+
+  '&[data-state="checked"]': {
+    backgroundColor: '$ignite300',
+  },
+})
+
+const slideIn = keyframes({
+  from: {
+    transform: 'translateY(-100%)',
+    opacity: 0,
+  },
+  to: {
+    transform: 'translateY(0)',
+    opacity: 1,
+  },
+})
+
+const slideOut = keyframes({
+  from: {
+    transform: 'translateY(0)',
+    opacity: 0,
+  },
+  to: {
+    transform: 'translateY(-100%)',
+    opacity: 1,
+  },
+})
+
+export const CheckboxIndicator = styled(Checkbox.Indicator, {
+  color: '$white',
+  width: '$4',
+  height: '$4',
+
+  '&[data-state="checked"]': {
+    animation: `${slideIn} 200ms ease-out`,
+  },
+
+  '&[data-state="unchecked"]': {
+    animation: `${slideOut} 200ms ease-out`,
+  },
+})
