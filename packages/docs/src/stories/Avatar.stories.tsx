3886e8e99e176f44782f61c38bf71f7d809569d5
capelaum <thecapellett@gmail.com>
2023-03-02 16:02:19 -0300

    ✨ component: Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,22 @@
+import {
+  Avatar,
+  AvatarProps,
+} from '@capelaum/ignite-react-05-design-system-react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Data Display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/capelaum.png',
+    alt: 'Capelaum',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const Fallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,17 @@
+import { UserCircle } from 'phosphor-react'
+import { ComponentProps } from 'react'
+import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+
+      <AvatarFallback delayMs={600}>
+        <UserCircle weight="fill" />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
