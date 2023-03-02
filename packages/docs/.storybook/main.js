beeaf1bcc62b2b7c0321b7e0fee1558cf6948d99
capelaum <thecapellett@gmail.com>
2023-03-02 18:56:19 -0300

    🐛 fix: Add subpath to storybook  on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -13,4 +13,11 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-react-design-system'
+    }
+
+    return config
+  },
 }
