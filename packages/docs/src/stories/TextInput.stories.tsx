653030d2408303e0595f45bc2f261f2031e93bd7
capelaum <thecapellett@gmail.com>
2023-03-02 16:44:52 -0300

    ✨ component: TextInput

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,45 @@
+import {
+  Box,
+  Text,
+  TextInput,
+  TextInputProps,
+} from '@capelaum/ignite-react-05-design-system-react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/Text Input',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Full Name</Text>
+          <Story />
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+    placeholder: 'Disabled input',
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
