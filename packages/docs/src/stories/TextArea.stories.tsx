c9adf86294b2ddb845109b6bb0d9c6b52bbfb388
capelaum <thecapellett@gmail.com>
2023-03-02 16:51:47 -0300

    ✨ component: TextArea

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,39 @@
+import {
+  Box,
+  Text,
+  TextArea,
+  TextAreaProps,
+} from '@capelaum/ignite-react-05-design-system-react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/TextArea',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Comment</Text>
+          <Story />
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Type your comment here',
+  },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+    placeholder: 'Disabled textarea',
+  },
+}
